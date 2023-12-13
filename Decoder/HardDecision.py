8657257469eaa9589bd328133aa401268a7f414f
etone61eric <etone61eric@outlook.com>
2023-12-14 00:30:41 +0100

    Add the (7,4) Hamming decoder.

--- Decoder/HardDecision.py
+++ Decoder/HardDecision.py
@@ -8,9 +8,9 @@ def hard_decision_BPSK(estimated_bits):
     tensor_1 = torch.tensor(1, device=mps_device)
     tensor_0 = torch.tensor(0, device=mps_device)
     estimated_bits = torch.where(estimated_bits > 0, tensor_1, tensor_0)
-    # estimated_bits = estimated_bits[:, :, 0:4]
 
     return estimated_bits
+
 
 def hard_decision_cutter(bitcodeword):
     r = torch.tensor([[0, 0, 1, 0, 0, 0, 0],
@@ -18,17 +18,20 @@ def hard_decision_cutter(bitcodeword):
                                [0, 0, 0, 0, 0, 1, 0],
                                [0, 0, 0, 0, 0, 0, 1], ],device=mps_device, dtype=torch.float)
 
-    result = torch.matmul(bitcodeword, r.T)
+
     tensor_1 = torch.tensor(1, device=mps_device)
     tensor_0 = torch.tensor(0, device=mps_device)
-    estimated_bits = torch.where(result > 0, tensor_1, tensor_0)
+    estimated_bits = torch.where(bitcodeword > 0, tensor_1, tensor_0)
+    result = torch.matmul(estimated_bits.to(torch.float), r.T).to(torch.int)
 
-    return estimated_bits
+    return result
 
-# input0 = torch.randn([10000000, 1, 7]).to(mps_device)
-# result = reverse_matrix(input0)
-# input1 = hard_decision_cutter(result)
+# # input0 = torch.randn([10000000, 1, 7]).to(mps_device)
+# input0 = torch.randint(0, 2, size = [1, 1, 7],dtype=int).to(mps_device)
 #
-#
-#
+# input1 = hard_decision_cutter(input0)
+# #
+# #
+# #
+# print("input0",input0)
 # print(input1)
