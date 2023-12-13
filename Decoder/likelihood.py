56ca6f46cfa6b09d64d8e34143c00c0155979d7d
etone61eric <etone61eric@outlook.com>
2023-12-13 18:20:52 +0100

    Adjust for LLR and it should be right now.

--- Decoder/likelihood.py
+++ Decoder/likelihood.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 mps_device = (torch.device("mps") if torch.backends.mps.is_available()
               else (torch.device("cuda") if torch.backends.cuda.is_available()
@@ -18,10 +19,29 @@ def llr(signal, snr):
     """
 
     # Assuming Binary Phase Shift Keying (BPSK) modulation
-    noise_std = torch.sqrt(torch.tensor(10 ** (snr / 10))).to(mps_device)
+
+    # p0 = snr / 10
+    # p1 = 10 ** p0
+    # p2 = 1/p1
+    # p3 = np.sqrt(p2)
+    # noise_std = p3
+
+    noise_std = torch.sqrt(torch.tensor((1.0)/(10 ** (snr / 10)))).to(mps_device)
 
     # Calculate the LLR
-    llr = 2 * signal * noise_std
+    llr = 2 * signal / (noise_std**2)
 
     # return llr_values, llr
     return llr
+
+# received_signal = torch.tensor(1)
+# snr_value = 10.0
+#
+# llr_values = llr(received_signal, snr_value)
+# print(f"LLR Values: {llr_values.item()}")
+#
+#
+# # received_signal = 1
+# # snr_value = 10
+# # llr_values = llr(received_signal, snr_value)
+# # print(llr_values)
