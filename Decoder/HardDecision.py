1c9b2f4af1d43992d06fb3ed0ade4544251f9627
etone61eric <etone61eric@outlook.com>
2023-12-14 13:20:20 +0100

    make hard decision independently

--- Decoder/HardDecision.py
+++ Decoder/HardDecision.py
@@ -12,19 +12,16 @@ def hard_decision_BPSK(estimated_bits):
     return estimated_bits
 
 
-def hard_decision_cutter(bitcodeword):
-    r = torch.tensor([[0, 0, 1, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 1, 0, 0],
-                               [0, 0, 0, 0, 0, 1, 0],
-                               [0, 0, 0, 0, 0, 0, 1], ],device=mps_device, dtype=torch.float)
+def hard_decision(bitcodeword):
+
 
 
     tensor_1 = torch.tensor(1, device=mps_device)
     tensor_0 = torch.tensor(0, device=mps_device)
     estimated_bits = torch.where(bitcodeword > 0, tensor_1, tensor_0)
-    result = torch.matmul(estimated_bits.to(torch.float), r.T).to(torch.int)
 
-    return result
+
+    return estimated_bits
 
 # # input0 = torch.randn([10000000, 1, 7]).to(mps_device)
 # input0 = torch.randint(0, 2, size = [1, 1, 7],dtype=int).to(mps_device)
