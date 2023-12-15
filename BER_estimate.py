862f7aa7934e573f3b9995ecb4ab24db92dd480c
etone61eric <etone61eric@outlook.com>
2023-12-15 18:42:39 +0100

    A new version of BP

--- BER_estimate.py
+++ BER_estimate.py
@@ -30,59 +30,59 @@ def main():
     result = np.zeros((3, len(SNR_opt_BPSK)))
     N = num
 
-    # # De-Encoder, BPSK only
-    # for i in range(len(SNR_opt_BPSK)):
-    #     snr_dB =SNR_opt_BPSK[i]
-    #
-    #     for j in range(10):
-    #         modulator = bpsk_modulator()
-    #
-    #         bits_info = generator(N).to(device)
-    #         modulated_signal = modulator(bits_info, device)
-    #         modulated_noise_signal = AWGN(modulated_signal, snr_dB, device)
-    #
-    #         BPSK_final = hard_decision(modulated_noise_signal, device)
-    #
-    #         BER_BPSK, error_num_BPSK= calculate_ber(BPSK_final, bits_info)
-    #         if error_num_BPSK < 100:
-    #             N += 10000000
-    #             print(f"the code number is {N}")
-    #
-    #         else:
-    #             print(f"BPSK: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_BPSK} and BER is {BER_BPSK}")
-    #             result[0, i] = BER_BPSK
-    #             break
+    # De-Encoder, BPSK only
+    for i in range(len(SNR_opt_BPSK)):
+        snr_dB =SNR_opt_BPSK[i]
+
+        for j in range(10):
+            modulator = bpsk_modulator()
+
+            bits_info = generator(N).to(device)
+            modulated_signal = modulator(bits_info, device)
+            modulated_noise_signal = AWGN(modulated_signal, snr_dB, device)
+
+            BPSK_final = hard_decision(modulated_noise_signal, device)
+
+            BER_BPSK, error_num_BPSK= calculate_ber(BPSK_final, bits_info)
+            if error_num_BPSK < 100:
+                N += 10000000
+                print(f"the code number is {N}")
+
+            else:
+                print(f"BPSK: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_BPSK} and BER is {BER_BPSK}")
+                result[0, i] = BER_BPSK
+                break
 
 
-    # # Maximum Likelihood
-    # for i in range(len(SNR_opt_ML)):
-    #     snr_dB = SNR_opt_ML[i]
-    #
-    #     for j in range(10):
-    #         encoder = hamming_encoder()
-    #         modulator = bpsk_modulator()
-    #         decoder = HammingDecoder(device)
-    #
-    #         # ML:
-    #         bits_info = generator(N)
-    #         encoded_codeword = encoder(bits_info).to(device)
-    #         modulated_signal = modulator(encoded_codeword, device)
-    #         modulated_noise_signal = AWGN(modulated_signal, snr_dB, device)
-    #
-    #         llr_output = llr(modulated_noise_signal, snr_dB)  # Log-Likelihood Calculation
-    #         HD_final = hard_decision(llr_output, device)
-    #         ML_final = decoder(HD_final)
-    #
-    #         BER_ML, error_num_ML = calculate_ber(ML_final, bits_info.to(device))
-    #         if error_num_ML < 100 & N <= 40000000:
-    #             N += 10000000
-    #             print(f"the code number is {N}")
-    #
-    #         else:
-    #             print(
-    #                 f"ML: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_ML} and BER is {BER_ML}")
-    #             result[1, i] = BER_ML
-    #             break
+    # Maximum Likelihood
+    for i in range(len(SNR_opt_ML)):
+        snr_dB = SNR_opt_ML[i]
+
+        for j in range(10):
+            encoder = hamming_encoder()
+            modulator = bpsk_modulator()
+            decoder = HammingDecoder(device)
+
+            # ML:
+            bits_info = generator(N)
+            encoded_codeword = encoder(bits_info).to(device)
+            modulated_signal = modulator(encoded_codeword, device)
+            modulated_noise_signal = AWGN(modulated_signal, snr_dB, device)
+
+            llr_output = llr(modulated_noise_signal, snr_dB)  # Log-Likelihood Calculation
+            HD_final = hard_decision(llr_output, device)
+            ML_final = decoder(HD_final)
+
+            BER_ML, error_num_ML = calculate_ber(ML_final, bits_info.to(device))
+            if error_num_ML < 100 & N <= 40000000:
+                N += 10000000
+                print(f"the code number is {N}")
+
+            else:
+                print(
+                    f"ML: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_ML} and BER is {BER_ML}")
+                result[1, i] = BER_ML
+                break
 
 
     # Belief Propagation
@@ -94,6 +94,7 @@ def main():
             encoder = hamming_encoder()
             modulator = bpsk_modulator()
             decoder = HammingDecoder(device)
+            ldpc_bp = LDPCBeliefPropagation(device)
 
             bits_info = generator(N)  # Code Generator
             encoded_codeword = encoder(bits_info).to(device) # Hamming(7,4) Encoder
@@ -101,8 +102,7 @@ def main():
             modulated_noise_signal = AWGN(modulated_signal, snr_dB, device) # Add Noise
 
             llr_output = llr(modulated_noise_signal, snr_dB).to(device) #LLR
-            ldpc_bp = LDPCBeliefPropagation(llr_output, device)
-            LDPC_result = ldpc_bp(iter) # LDPC
+            LDPC_result = ldpc_bp(llr_output, iter) # LDPC
             LDPC_HD = hard_decision(LDPC_result, device) #Hard Decision
             LDPC_final = decoder(LDPC_HD)
 
@@ -135,6 +135,7 @@ if __name__ == "__main__":
     result_all = np.zeros((3, 10))
 
     result_all = main()
+    print(result_all)
 
     # Create the Plot
     plt.semilogy(result_all.T,marker='*')
--- Decoder/LDPC_BP.py
+++ Decoder/LDPC_BP.py
@@ -1,55 +1,94 @@
 import torch
 
 class LDPCBeliefPropagation(torch.nn.Module):
-    def __init__(self, llr, mps_device):
+    def __init__(self, device):
         """
         LDPC Belief Propagation.
 
         Args:
             H: Low density parity code for building tanner graph.
             llr: Log Likelihood Ratio (LLR) values. Only for 7-bit codeword.
-            mps_device: Move Data on Specific device for computing(GPU).
 
         Returns:
             estimated_bits: the output result from belief propagation.
         """
 
         super(LDPCBeliefPropagation, self).__init__()
-        self.llr = llr
         self.H = torch.tensor([[1, 1, 1, 0, 0, 0, 0],
                                [0, 0, 1, 1, 1, 0, 0],
                                [0, 1, 0, 0, 1, 1, 0],
-                               [1, 0, 0, 1, 0, 0, 1], ], device=mps_device)
+                               [1, 0, 0, 1, 0, 0, 1], ],dtype=torch.float, device=device)
         self.num_check_nodes, self.num_variable_nodes = self.H.shape
-        self.channel = llr.shape[2]
 
+    def forward(self, llr, max_iters):
         # Initialize messages
-        self.messages_v_to_c = torch.ones((self.num_variable_nodes, self.num_check_nodes, self.channel),
-                                          dtype=torch.float).to(mps_device)
-        self.messages_c_to_v = torch.zeros((self.num_check_nodes, self.num_variable_nodes, self.channel),
-                                           dtype=torch.float).to(mps_device)
+        messages_v_to_c = llr.repeat(1, self.num_check_nodes, 1) * self.H
+        print(messages_v_to_c)
+        messages_c_to_v = torch.zeros_like(messages_v_to_c)
 
-    def forward(self, max_iter,):
-        for iteration in range(max_iter):
+        for iteration in range(max_iters):
+            # Variable Node Update
+            messages_v_to_c = self.variable_node_update(llr, messages_c_to_v)
 
-            # Variable to check node messages
-            for i in range(self.num_variable_nodes):
-                for j in range(self.num_check_nodes):
-                    # Compute messages from variable to check nodes
-                    connected_checks = self.H[j, :] == 1
-                    product = torch.prod(torch.tanh(0.5 * self.messages_v_to_c[connected_checks, j]), dim=0,
-                                         keepdim=True)
-                    self.messages_v_to_c[i, j] = torch.sign(self.llr[j]) * product
+            # Check Node Update
+            messages_c_to_v = self.check_node_update(messages_v_to_c)
 
-            # Check to variable node messages
-            for i in range(self.num_check_nodes):
-                for j in range(self.num_variable_nodes):
-                    # Compute messages from check to variable nodes
-                    connected_vars = self.H[:, j] == 1
-                    sum_msgs = torch.sum(self.messages_c_to_v[connected_vars, i]) - self.messages_v_to_c[j, i]
-                    self.messages_c_to_v[i, j] = 2 * torch.atan(torch.exp(0.5 * sum_msgs))
+        # Final decision
+        sums = messages_c_to_v.sum(dim=1).unsqueeze(1)
+        llr_final = llr + sums
+        return llr_final
 
-        # Calculate the final estimated bits and only take first four bits
-        estimated_bits = torch.sign(self.llr) * torch.prod(torch.tanh(0.5 * self.messages_c_to_v))
+    def variable_node_update(self, llr, messages_c_to_v):
+        # Update messages from variable nodes to check nodes
+        total = llr + messages_c_to_v.sum(dim=1, keepdim=True)
+        result = total - messages_c_to_v
 
-        return estimated_bits
+        return result
+
+    def check_node_update(self, messages_v_to_c):
+        # # Update messages from check nodes to variable nodes
+        alpha = torch.sign(messages_v_to_c)
+
+        # Beta represents the absolute value of each message
+        beta = torch.abs(messages_v_to_c)
+
+        # Sum of phi(beta_ij) for all i' (excluding the current variable i)
+        # # We temporarily set the current variable's message to zero to exclude it from the sum
+        # original_beta = beta.clone()
+        # beta[:, :] = 0
+        sum_phi_beta = self.phi(beta).sum(dim=1, keepdim=True)
+
+        # # Now we restore the original beta values
+        # beta = original_beta
+        #
+        # # Compute the phi of sum_phi_beta, which is the same for all edges connected to the check node
+        # phi_sum_phi_beta = self.phi(sum_phi_beta)
+
+        # The final message is the product of alpha and the phi of the sum of phi(beta)
+        # for each edge, we exclude the current variable's contribution by subtracting its phi(beta)
+        # messages_c_to_v = alpha * (phi_sum_phi_beta - self.phi(beta))
+        messages_c_to_v = torch.prod(alpha) * self.phi(sum_phi_beta)
+
+        # return result
+        return messages_c_to_v
+
+    def phi(self, x):
+        # Avoid division by zero and numerical instability when x is very small
+        return -torch.log(torch.tanh(x / 2))
+
+
+device = torch.device("mps")
+
+llr_output = torch.tensor([[[1, 1, 1, 1, 1, 1, 1]],
+                           [[-1, -2, -3, -4, -5, -6, -7]], ], dtype=torch.float,
+                          device=device)  # torch.Size([2, 1, 7])
+#
+# # result = torch.sum(llr_output, dim=1)
+# # print(result)
+#
+iter = 5
+
+ldpc_bp = LDPCBeliefPropagation(device)
+LDPC_result = ldpc_bp(llr_output, iter)  # LDPC
+# print(llr_output)
+# print(LDPC_result)
