db03eaab894ead6077a4029fc7bf75d9322a2376
etone61eric <etone61eric@outlook.com>
2023-12-19 15:17:03 +0100

    moving encoder to GPU

--- Encoder/Hamming74.py
+++ Encoder/Hamming74.py
@@ -2,7 +2,7 @@ import torch
 
 # Hamming(7,4) Encoder
 class hamming_encoder(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, device):
         """
         Use Hamming(7,4) to encode the data.
 
@@ -23,19 +23,22 @@ class hamming_encoder(torch.nn.Module):
             [0, 1, 1, 1],
             [0, 1, 0, 0],
             [0, 0, 1, 0],
-            [0, 0, 0, 1],], dtype=torch.int)
+            [0, 0, 0, 1],], dtype=torch.float, device=device)
 
     def forward(self, input_data):
         # Ensure input_data has shape (batch_size)
         # assert input_data.size(0) == self.generator_matrix.shape[1], "Input data must have same generator matrix row number bits."
 
         # Perform matrix multiplication to encode the data
-        result_tensor = torch.matmul(input_data, self.generator_matrix.t()) % 2
+        result_tensor = torch.matmul(input_data.to(torch.float), self.generator_matrix.t()) % 2
 
-        return result_tensor
+        return result_tensor.to(torch.int)
 
+# device = (torch.device("mps") if torch.backends.mps.is_available()
+#                                     else (torch.device("cuda") if torch.backends.cuda.is_available()
+#                                           else torch.device("cpu")))
 # bits = torch.tensor([[[1, 1, 0, 1]],
-#                          [[0, 1, 0, 0,]]], dtype=torch.int32)
+#                          [[0, 1, 0, 0,]]], dtype=torch.int, device=device)
 #
-# encoder = hamming_encoder()
+# encoder = hamming_encoder(device)
 # print(encoder(bits))
