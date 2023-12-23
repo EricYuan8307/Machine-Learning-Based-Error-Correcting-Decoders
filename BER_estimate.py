7ee044b3e33e87024d7d9928ce157b5ae55f73fd
etone61eric <etone61eric@outlook.com>
2023-12-23 21:33:19 +0100

    Add hard decision line

--- BER_estimate.py
+++ BER_estimate.py
@@ -1,14 +1,13 @@
-import time
-
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+import time
 
 from Encoder.BPSK import bpsk_modulator
 from Encoder.Hamming74 import hamming_encoder
-from Decoder.likelihood import llr
-from Decoder.LDPC_BP import LDPCBeliefPropagation
 from Decoder.HardDecision import hard_decision
+from Decoder.LDPC_BP import LDPCBeliefPropagation
+from Decoder.likelihood import llr
 from Transmit.noise import AWGN
 from Estimation.BitErrorRate import calculate_ber
 from Decoder.HammingDecoder import Hamming74decoder
@@ -26,11 +25,11 @@ def generator(nr_codewords, device):
 # Calculate the Error number and BER
 def main():
     SNR_opt_BPSK = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    SNR_opt_ML = [0, 1, 2, 3, 4, 5, 6,]
-    SNR_opt_BP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # SNR_opt_BP = [1]
+    SNR_opt_ML = [0, 1, 2, 3, 4, 5, 6, 7]
+    # SNR_opt_BP = [0, 1, 2, 3, 4, 5, 6, 7]
+    SNR_opt_BP = [6, 7]
 
-    result = np.zeros((3, len(SNR_opt_BPSK)))
+    result = np.zeros((4, len(SNR_opt_BPSK)))
     N = num
 
     # De-Encoder, BPSK only
@@ -76,15 +75,46 @@ def main():
             HD_final = hard_decision(SD_ML, device)
             SDML_final = decoder(HD_final)
 
-            BER_ML, error_num_ML = calculate_ber(SDML_final, bits_info)
-            if error_num_ML < 100 & N <= 40000000: # Have some problems especially after the SNR >= 6, the error number is 65 and Signal number do not update.
+            BER_SDML, error_num_SDML = calculate_ber(SDML_final, bits_info)
+            if error_num_SDML < 100 & N <= 40000000: # Have some problems especially after the SNR >= 6, the error number is 65 and Signal number do not update.
                 N += 1000000
                 print(f"the code number is {N}")
 
             else:
                 print(
-                    f"ML: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_ML} and BER is {BER_ML}")
-                result[1, i] = BER_ML
+                    f"SD-ML: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_SDML} and BER is {BER_SDML}")
+                result[1, i] = BER_SDML
+                break
+
+    # Hard-Decision Maximum Likelihood
+    for i in range(len(SNR_opt_ML)):
+        snr_dB = SNR_opt_ML[i]
+        N = num
+
+        for l in range(10):
+            encoder = hamming_encoder(device)
+            HD_MaximumLikelihood = HardDecisionML(device)
+            decoder = Hamming74decoder(device)
+
+            # ML:
+            bits_info = generator(N, device)
+            encoded_codeword = encoder(bits_info)
+            modulated_signal = bpsk_modulator(encoded_codeword)
+            noised_signal = AWGN(modulated_signal, snr_dB, device)
+
+            HD_signal = hard_decision(noised_signal, device)
+            HD_ML = HD_MaximumLikelihood(HD_signal)
+            HDML_final = decoder(HD_ML)
+
+            BER_HDML, error_num_HDML = calculate_ber(HDML_final, bits_info)
+            if error_num_HDML < 100 & N <= 40000000:  # Have some problems especially after the SNR >= 6, the error number is 65 and Signal number do not update.
+                N += 1000000
+                print(f"the code number is {N}")
+
+            else:
+                print(
+                    f"HD-ML: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_HDML} and BER is {BER_HDML}")
+                result[2, i] = BER_HDML
                 break
 
 
@@ -107,23 +137,23 @@ def main():
             noised_signal = AWGN(modulated_signal, snr_dB, device) # Add Noise
 
             llr_output = llr(noised_signal, snr_dB) #LLR
-            result = torch.zeros(llr_output.shape)
+            BP_result = torch.zeros(llr_output.shape, device=device)
             for k in range(llr_output.shape[0]):
                 start_time = time.time()
 
                 BP = ldpc_bp(llr_output[k], iter) # LDPC
-                result[k] = BP
+                BP_result[k] = BP
                 end_time = time.time()
 
-                if k % 1000 ==0 and k >0:
-                    elapsed_time = time.time() - start_time
-                    print(f"Processed {k} iterations in {elapsed_time*1000} seconds")
+                if k % 10000 ==0 and k >0:
+                    elapsed_time = end_time - start_time
+                    print(f"Processed {k} iterations in {elapsed_time*10000} seconds")
 
             iter_end_time = time.time()
 
             print(f"For {snr_dB}SNR, the Belief Propagation spend {iter_end_time-iter_start_time} seconds.")
 
-            LDPC_HD = hard_decision(result, device) # Hard Decision
+            LDPC_HD = hard_decision(BP_result, device) # Hard Decision
 
             LDPC_final = decoder(LDPC_HD) # Decoder
 
@@ -135,7 +165,7 @@ def main():
 
             else:
                 print(f"LDPC: When SNR is {snr_dB} and signal number is {N}, error number is {error_num_LDPC} and BER is {BER_LDPC}")
-                result[2, i] = BER_LDPC
+                result[3, i] = BER_LDPC
                 break
 
 
@@ -149,7 +179,7 @@ if __name__ == "__main__":
                                           else torch.device("cpu")))
 
     #Hpyer parameters
-    num = int(1e6) #how many original need to generate
+    num = int(1e7) #how many original need to generate
     iter = 5 # LDPC Belief Propagation iteration time
 
     # Store result for plot
@@ -164,7 +194,7 @@ if __name__ == "__main__":
     plt.xlabel('SNR')
     plt.ylabel('BER')
     plt.title('Estimation')
-    plt.legend(['Uncoded-BPSK', 'Soft-Decision ML', 'Belief Propagation'])
+    plt.legend(['Uncoded-BPSK', 'Soft-Decision ML', 'Hard-Decision ML', 'Belief Propagation'])
 
     # Display the Plot
     plt.show()
