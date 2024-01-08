b14d0b6e15ea5ebbaff0f7b255b942111f828208
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-08 16:11:12 +0100

    device update

--- BER_estimate.py
+++ BER_estimate.py
@@ -197,10 +197,10 @@ def BeliefPropagation(nr_codeword, snr_dB, device):
 
 
 if __name__ == "__main__":
-    # device = (torch.device("mps") if torch.backends.mps.is_available()
-    #                                 else (torch.device("cuda") if torch.backends.cuda.is_available()
-    #                                       else torch.device("cpu")))
-    device = torch.device("cpu")
+    device = (torch.device("mps") if torch.backends.mps.is_available()
+                                    else (torch.device("cuda") if torch.backends.cuda.is_available()
+                                          else torch.device("cpu")))
+    # device = torch.device("cpu")
 
     #Hpyer parameters
     num = int(1e7) #how many original need to generate
