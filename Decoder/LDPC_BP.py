3ac2df864ba57514ca3a40232db5e8a450a3d227
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-09 15:18:27 +0100

    For the one iteration, it works well and result is correct

--- Decoder/LDPC_BP.py
+++ Decoder/LDPC_BP.py
@@ -15,16 +15,19 @@ class LDPCBeliefPropagation(torch.nn.Module):
         """
 
         super(LDPCBeliefPropagation, self).__init__()
-        self.H = torch.tensor([[[1, 0, 1, 0, 1, 0, 1],
-                               [0, 1, 1, 0, 0, 1, 1],
-                               [0, 0, 0, 1, 1, 1, 1], ], ],dtype=torch.float, device=device)
+        # self.H = torch.tensor([[[1, 0, 1, 0, 1, 0, 1],
+        #                        [0, 1, 1, 0, 0, 1, 1],
+        #                        [0, 0, 0, 1, 1, 1, 1], ], ],dtype=torch.float64, device=device)
+        self.H = torch.tensor([[[1, 1, 0, 1, 1, 0, 0],
+                                [1, 0, 1, 1, 0, 1, 0],
+                                [0, 1, 1, 1, 0, 0, 1], ], ], dtype=torch.float64, device=device)
         self.num_check_nodes = self.H.shape[1]
         self.num_variable_nodes = self.H.shape[2]
         self.device = device
 
     def forward(self, llr, max_iters):
         # Initial values
-        messages_v_to_c = torch.zeros(llr.shape, dtype=torch.float, device=self.device)
+        messages_v_to_c = torch.zeros(llr.shape, dtype=torch.float64, device=self.device)
         # print("initial llr", llr)
 
         for iteration in range(max_iters):
@@ -49,7 +52,7 @@ class LDPCBeliefPropagation(torch.nn.Module):
             sum_messages_c_to_v = torch.sum(messages_c_to_v,dim=1)
             final_llr = llr + sum_messages_c_to_v
 
-            est = hard_decision(final_llr, self.device).to(torch.float) # torch.Size([1, 1, 7])
+            est = hard_decision(final_llr, self.device).to(torch.float64) # torch.Size([1, 1, 7])
             mult = torch.matmul(est, self.H.transpose(1, 2))%2
 
             if torch.all(mult == torch.zeros(mult.shape, device=self.device)):
@@ -68,31 +71,14 @@ class LDPCBeliefPropagation(torch.nn.Module):
         return result
 
 
-device = torch.device("cpu")
-
-llr_output = torch.tensor([[[6.5670, 8.5779, -2.2217, -5.2829, -8.0088, -0.2273, 2.9470]],
-                           # [[  92.018,  -20.977,  -13.301, -176.342, -154.045,  -58.012,  -11.695]],
-                           ], dtype=torch.float,device=device)  # torch.Size([2, 1, 7])
-# nr_number = int(1e4)
-# llr_output = torch.randn(nr_number, 1, 7, dtype=torch.float, device=device)
-# # print("llr_output", llr_output)
-result = torch.zeros(llr_output.shape)
-
-for i in range(2):
-    input = llr_output[i]
-    iter = 5
-    ldpc_bp = LDPCBeliefPropagation(device)
-    LDPC_result = ldpc_bp(input, iter) # LDPC
-    result[i] = LDPC_result
-
-# print(result)
+# device = torch.device("cpu")
+#
+# llr_output = torch.tensor([[[6.5670, 8.5779, -2.2217, -5.2829, -8.0088, -0.2273, 2.9470]]], dtype=torch.float,device=device)
+# # llr_output = torch.tensor([[[20.4128, -16.3902, -19.1344, -15.7405, -26.6343, 23.9271, 21.8500]]], dtype=torch.float,device=device)  # torch.Size([2, 1, 7])
+#
+# iter = 5
+# ldpc_bp = LDPCBeliefPropagation(device)
+# LDPC_result = ldpc_bp(llr_output, iter) # LDPC
+#
 #
 # print("LDPC_result: ", LDPC_result)
-
-HD_result = hard_decision(LDPC_result, device)
-HD_input = hard_decision(llr_output, device)
-
-
-print("HD_input", HD_input.shape)
-print("HD_result", HD_result.shape)
-
