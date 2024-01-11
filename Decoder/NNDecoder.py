404778f562496146670175272de27c1dfd9f8153
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-11 15:35:03 +0100

    The Neural Network works but acts very bad

--- Decoder/NNDecoder.py
+++ Decoder/NNDecoder.py
@@ -7,6 +7,7 @@ class SingleLableNNDecoder(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
 
         super().__init__()
+        self.input_size = input_size
         self.hidden = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.output = nn.Linear(hidden_size, output_size)
@@ -16,7 +17,7 @@ class SingleLableNNDecoder(nn.Module):
         x = self.hidden(x)
         x = self.relu(x)
         x = self.output(x)
-        x = x.view(x.shape[0], input_size, 2) # torch.Size([5, 1, 14]) to torch.Size([5, 7, 2])
+        x = x.view(x.shape[0], self.input_size, 2) # torch.Size([5, 1, 14]) to torch.Size([5, 7, 2])
         x = self.softmax(x)
         x = torch.argmax(x, dim=2).unsqueeze(1).to(torch.float).requires_grad_(True) # torch.Size([5, 1, 7])
 
@@ -26,25 +27,25 @@ class SingleLableNNDecoder(nn.Module):
 # device = (torch.device("mps") if torch.backends.mps.is_available()
 #           else (torch.device("cuda") if torch.backends.cuda.is_available()
 #                 else torch.device("cpu")))
-device = torch.device("cpu")
-
-# Define your dataset and dataloader
-# Example data and labels (replace with your own dataset)
-nr = 5
-data = torch.randn(size=(nr, 1, 7), dtype=torch.float, device=device, requires_grad=True)
-# data = torch.ones(size=(2, 1, 1), dtype=torch.float, device=device)
-# data = torch.tensor([[[2, 0], [0, 1]],
-#                      # [[0, 1],[0, 1]],
-#                      ], dtype=torch.float, device=device)
-print("data shape: ", data.shape)
-labels = torch.randint(0, 2, size=(nr, 1, 7), dtype=torch.float, device=device)
-
-# Hyperparameters
-input_size = data.shape[2]
-hidden_size = data.shape[2]
-output_size = 2*data.shape[2]
-learning_rate = 0.01
-epochs = 10000
+# device = torch.device("cpu")
+#
+# # Define your dataset and dataloader
+# # Example data and labels (replace with your own dataset)
+# nr = 5
+# data = torch.randn(size=(nr, 1, 7), dtype=torch.float, device=device, requires_grad=True)
+# # data = torch.ones(size=(2, 1, 1), dtype=torch.float, device=device)
+# # data = torch.tensor([[[2, 0], [0, 1]],
+# #                      # [[0, 1],[0, 1]],
+# #                      ], dtype=torch.float, device=device)
+# print("data shape: ", data.shape)
+# labels = torch.randint(0, 2, size=(nr, 1, 7), dtype=torch.float, device=device)
+#
+# # Hyperparameters
+# input_size = data.shape[2]
+# hidden_size = data.shape[2]
+# output_size = 2*data.shape[2]
+# learning_rate = 0.01
+# epochs = 10000
 
 
 
--- NetworkTraining/LabelNN_Training.py
+++ NetworkTraining/LabelNN_Training.py
@@ -6,44 +6,67 @@ from Encoder.Generator import generator
 from Encoder.BPSK import bpsk_modulator
 from Encoder.Hamming74 import hamming_encoder
 from Transmit.noise import AWGN
-from Estimation.BitErrorRate import calculate_ber
-from Decoder.HammingDecoder import Hamming74decoder
 from Decoder.NNDecoder import SingleLableNNDecoder
 from Transmit.NoiseMeasure import NoiseMeasure
 
-device = (torch.device("mps") if torch.backends.mps.is_available()
-                                    else (torch.device("cuda") if torch.backends.cuda.is_available()
-                                          else torch.device("cpu")))
+
+def main():
+    snr = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
+
+    for i in range(len(snr)):
+        snr_dB = snr[i]
+        encoder = hamming_encoder(device)
+
+        # ML:
+        bits_info = generator(nr_codeword, device)
+        encoded_codeword = encoder(bits_info).to(torch.float)
+        modulated_signal = bpsk_modulator(encoded_codeword)
+        noised_signal = AWGN(modulated_signal, snr_dB, device)
+
+        practical_snr = NoiseMeasure(noised_signal, modulated_signal)
+
+        # Create an instance of the SimpleNN class
+        model = SingleLableNNDecoder(noised_signal.shape[2], hidden_size, 2*noised_signal.shape[2]).to(device)
+
+        # Define the loss function and optimizer
+        criterion = nn.BCELoss()  # Binary Cross Entropy Loss for binary classification
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+        # Training loop
+        for epoch in range(epochs):
+            # Forward pass
+            outputs = model(noised_signal)
+
+            # Compute the loss
+            loss = criterion(outputs, encoded_codeword)
+
+            # Backpropagation and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (epoch + 1) % 1000 == 0:
+                print(f'When SNR is {practical_snr}, Epoch [{epoch + 1}/{epochs}], BER: {loss.item()/100}')
+
+        # # Testing the model
+        # with torch.no_grad():
+        #     test_data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
+        #     predicted_labels = model(test_data)
+        #     predicted_labels = (predicted_labels > 0.5).float()  # Threshold the outputs
+        #
+        #     print("Predicted labels:")
+        #     print(predicted_labels)
+
+if __name__ == '__main__':
+    device = (torch.device("mps") if torch.backends.mps.is_available()
+              else (torch.device("cuda") if torch.backends.cuda.is_available()
+                    else torch.device("cpu")))
     # device = torch.device("cpu")
 
-# Create an instance of the SimpleNN class
-model = SingleLableNNDecoder(input_size, hidden_size, output_size).to(device)
+    # Hyperparameters
+    hidden_size = 7
+    learning_rate = 0.01
+    epochs = 10000
+    nr_codeword = int(1e5)
 
-# Define the loss function and optimizer
-criterion = nn.BCELoss()  # Binary Cross Entropy Loss for binary classification
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-# Training loop
-for epoch in range(epochs):
-    # Forward pass
-    outputs = model(data)
-
-    # Compute the loss
-    loss = criterion(outputs, labels)
-
-    # Backpropagation and optimization
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    if (epoch + 1) % 1000 == 0:
-        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item()}')
-
-# # Testing the model
-# with torch.no_grad():
-#     test_data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
-#     predicted_labels = model(test_data)
-#     predicted_labels = (predicted_labels > 0.5).float()  # Threshold the outputs
-#
-#     print("Predicted labels:")
-#     print(predicted_labels)
+    main()
