3f8c7a984e6a8bc6d44cfd510f15d3a041257beb
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-11 15:53:01 +0100

    change from int to float

--- Encoder/Generator.py
+++ Encoder/Generator.py
@@ -1,7 +1,6 @@
 import torch
 
 def generator(nr_codewords, device):
-    bits = torch.randint(0, 2, size=(nr_codewords, 1, 4), dtype=torch.int, device=device)
-    # bits = torch.tensor([[[1, 1, 0, 1]], [[0, 1, 0, 0,]]], dtype=torch.int, device=device)
+    bits = torch.randint(0, 2, size=(nr_codewords, 1, 4), dtype=torch.float, device=device)
 
     return bits
