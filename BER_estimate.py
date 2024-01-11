2ee14342ba4cae65dc00cc0f22d75b2b20ae2075
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-11 17:17:35 +0100

    Change structure

--- BER_estimate.py
+++ BER_estimate.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import time
 import os
-import matplotlib.pyplot as plt
+from datetime import datetime
 
 from Encoder.Generator import generator
 from Encoder.BPSK import bpsk_modulator
@@ -18,11 +18,7 @@ from Transmit.NoiseMeasure import NoiseMeasure
 
 
 # Calculate the Error number and BER
-def main():
-    SNR_opt_BPSK = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
-    SNR_opt_ML = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
-    SNR_opt_BP = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9]
-
+def estimation(num, iter, SNR_opt_BPSK, SNR_opt_ML, SNR_opt_BP, device):
 
     result = np.zeros((4, len(SNR_opt_BPSK)))
     N = num
@@ -88,7 +84,7 @@ def main():
         snr_dB = SNR_opt_BP[i]
 
         for _ in range(10):
-            LDPC_final, bits_info, snr_measure = BeliefPropagation(N, snr_dB, device)
+            LDPC_final, bits_info, snr_measure = BeliefPropagation(N, snr_dB, iter, device)
 
             BER_LDPC, error_num_LDPC = calculate_ber(LDPC_final, bits_info) # BER calculation
 
@@ -155,7 +151,7 @@ def HardDecisionMLP(nr_codeword, snr_dB, device):
 
     return HDML_final, bits_info, practical_snr
 
-def BeliefPropagation(nr_codeword, snr_dB, device):
+def BeliefPropagation(nr_codeword, snr_dB, iter, device):
     iter_start_time = time.time()
 
     encoder = hamming_encoder(device)
@@ -191,66 +187,35 @@ def BeliefPropagation(nr_codeword, snr_dB, device):
 
     return LDPC_final, bits_info, practical_snr
 
-
-
-if __name__ == "__main__":
+def main():
     device = (torch.device("mps") if torch.backends.mps.is_available()
-                                    else (torch.device("cuda") if torch.backends.cuda.is_available()
-                                          else torch.device("cpu")))
+              else (torch.device("cuda") if torch.backends.cuda.is_available()
+                    else torch.device("cpu")))
     # device = torch.device("cpu")
 
-    #Hpyer parameters
+    # Hpyer parameters
     num = int(2e7)
     iter = 5
+    SNR_opt_BPSK = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+    SNR_opt_ML = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
+    SNR_opt_BP = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9]
 
-    result_all = main()
-    print(result_all)
+    result_all = estimation(num, iter, SNR_opt_BPSK, SNR_opt_ML, SNR_opt_BP, device)
 
-    directory_path = "Result"
-    csv_filename = "result.csv"
+    directory_path = "Result/BER"
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    # Get the current timestamp as a string
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Construct the filename with the timestamp
+    csv_filename = f"BER_result_{current_time}.csv"
+
     full_csv_path = os.path.join(directory_path, csv_filename)
     np.savetxt(full_csv_path, result_all, delimiter=',')
 
-    # Data Storage Reference:
-    ref_HDML = [0.04368, 0.0331875, 0.02469, 0.01624, 0.01130, 0.00734, 0.00444, 0.00235, 0.00137, 0.000725, 0.000290,
-                0.000141944, 4.81731e-05, 1.84191e-05, 5.37554e-06, 1.35552e-06, 3.68058e-07, 6.24751e-08, 9.625e-09,
-                1.225e-09, 1.25e-10]
 
-    ref_SDML = [0.0209, 0.014785, 0.00904, 0.0058325, 0.003405, 0.001765, 0.00085975, 0.000385625, 0.000161875,
-                6.10714e-05,2.0875e-05, 5.61659e-06, 1.71214e-06, 3.8256e-07, 6.3039e-08, 9.675e-09, 8.25e-10, 0,
-                0, 0, 0]
-    ref_BP = [0.026185, 0.01830, 0.01246, 0.0076925, 0.0048225, 0.0026775, 0.0013025, 0.0006325, 0.00029950,0.000122045,
-              4.875e-05, 1.45349e-05, 4.28082e-06, 1.15101e-06, 2.38981e-07, 3.95057e-08, 6.35e-09, 7e-10, 5e-11, 0, 0]  # ref BP
-
-    uncoded_BPSK = [0.07864588, 0.0670532, 0.05630509, 0.046395885, 0.03750538, 0.02963852, 0.02287882,
-                    0.01716848, 0.012505335, 0.00879166, 0.00595442, 0.003866035, 0.00238995, 0.00140195,
-                    0.00076981, 0.000398745, 0.000193015, 8.405e-05, 3.335e-05, 1.21e-05, 3.855e-06]  # Uncoded BPSK
-    SDML = [0.021438075, 0.014477825, 0.009354125, 0.0056875, 0.003245525, 0.001715425, 0.000867975, 0.000398725,
-            0.00016595, 5.9475e-05, 2.1025e-05, 6.275e-06, 1.3875e-06, 3.675e-07, 0.00000000e+00, 0.00000000e+00,
-            0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00]
-    HDML = [0.04415109, 0.033231805, 0.0241618475, 0.0169061, 0.0113552825, 0.0072749425, 0.00440249,
-            0.0025212025, 0.0013590475, 0.0006786375, 0.0003117775, 0.0001306225, 5.03725e-05, 1.759e-05,
-            5.2725e-06, 1.4225e-06, 3.425e-07, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00]
-    BP = [0.0265, 0.018725, 0.013625, 0.0079, 0.0048, 0.003075, 0.0015113636363636364,
-          7.136363636363637e-04, 2.659090909090909e-04, 1.2016129032258065e-04, 6.470588235294117e-05, 1.755e-05,
-          6.3e-06,
-          1.685e-06, 0, 0, 0, 0, 0, 0, 0]
-
-    SNR = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
-
-    plt.figure(figsize=(10, 10))
-    plt.semilogy(SNR, ref_HDML, marker='*', label='ref_HDML')
-    plt.semilogy(SNR, ref_SDML, marker='x', label='ref_SDML')
-    plt.semilogy(SNR, ref_BP, marker='+', label='ref_BP')
-    plt.semilogy(SNR, uncoded_BPSK, marker='.', label='uncoded_BPSK')
-    plt.semilogy(SNR, HDML, marker='*', label='HDML')
-    plt.semilogy(SNR, SDML, marker='x', label='SDML')
-    plt.semilogy(SNR, BP, marker='+', label='BP')
-
-    plt.xlabel('SNR')
-    plt.ylabel('BER')
-    plt.title('BER Estimation')
-    plt.legend(['ref_HDML', 'ref_SDML', 'ref_BP', 'uncoded_BPSK', 'HDML', 'SDML', "BP"], loc='lower left')
-
-    # Display the Plot
-    plt.show()
+if __name__ == "__main__":
+    main()
--- Decoder/NNDecoder.py
+++ Decoder/NNDecoder.py
@@ -33,17 +33,14 @@ from Transmit.noise import AWGN
 from Decoder.NNDecoder import SingleLableNNDecoder
 from Transmit.NoiseMeasure import NoiseMeasure
 
-
-def main():
-    # snr = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
-    snr = [4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
+def training(snr, nr_codeword, epochs, learning_rate, hidden_size, device):
 
     for i in range(len(snr)):
         snr_dB = snr[i]
-        print("snr_dB = ", snr_dB)
+
+        # Encoder:
         encoder = hamming_encoder(device)
 
-        # ML:
         bits_info = generator(nr_codeword, device)
         encoded_codeword = encoder(bits_info)
         modulated_signal = bpsk_modulator(encoded_codeword)
@@ -74,16 +71,34 @@ def main():
             if (epoch + 1) % 1000 == 0:
                 print(f'When SNR is {practical_snr}, Epoch [{epoch + 1}/{epochs}], BER: {loss.item()/100}')
 
-        # # Testing the model
-        # with torch.no_grad():
-        #     test_data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
-        #     predicted_labels = model(test_data)
-        #     predicted_labels = (predicted_labels > 0.5).float()  # Threshold the outputs
-        #
-        #     print("Predicted labels:")
-        #     print(predicted_labels)
+    #     # Testing the model
+    #     with torch.no_grad():
+    #         test_data = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
+    #         predicted_labels = model(test_data)
+    #         predicted_labels = (predicted_labels > 0.5).float()  # Threshold the outputs
+    #
+    #         print("Predicted labels:")
+    #         print(predicted_labels)
+    #
+    # def img_loss(train_losses, test_losses, file_name):
+    #     num_epochs = len(train_losses)
+    #     place = "images/" + file_name
+    #     # plot the loss
+    #     fig, ax = plt.subplots(figsize=(12, 6))
+    #     ax.plot(train_losses, label='Training loss')
+    #     ax.plot(test_losses, label='Testing loss')
+    #     ax.set_xlim(0, num_epochs - 1)
+    #
+    #     # axis labels
+    #     plt.xlabel('Epoch')
+    #     plt.ylabel('Loss')
+    #     plt.legend()
+    #     plt.savefig(place)
+    #     plt.show()
 
-if __name__ == '__main__':
+def main():
+    # snr = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
+    snr = [4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5]
     # device = (torch.device("mps") if torch.backends.mps.is_available()
     #           else (torch.device("cuda") if torch.backends.cuda.is_available()
     #                 else torch.device("cpu")))
@@ -95,4 +110,9 @@ if __name__ == '__main__':
     epochs = 10000
     nr_codeword = int(40)
 
+    training(snr, nr_codeword, epochs, learning_rate, hidden_size, device)
+
+
+
+if __name__ == '__main__':
     main()
