d13c007d2d3583ac5d068cb5237cb27c046fc3b4
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-16 14:58:41 +0100

    fininshed the MLNN

--- Decoder/NNDecoder.py
+++ Decoder/NNDecoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class SingleLableNNDecoder(nn.Module):
+class SingleLabelNNDecoder(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
 
         super().__init__()
@@ -22,3 +22,20 @@ class SingleLableNNDecoder(nn.Module):
         return x
 
 
+class MultiLabelNNDecoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+
+        super().__init__()
+        self.input_size = input_size
+        self.hidden = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.output = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.hidden(x)
+        x = self.relu(x)
+        x = self.output(x)
+        x = self.sigmoid(x)
+
+        return x
