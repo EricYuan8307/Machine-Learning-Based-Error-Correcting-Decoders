ea93f2a6271f2bf15c927651633100ebf94bbed3
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-19 00:30:06 +0100

    Fix the bugs in Converter.py

--- Decoder/Converter.py
+++ Decoder/Converter.py
@@ -1,6 +1,16 @@
 import torch
 import torch.nn as nn
 
+
+# Multi-label Neural Network Decoder:
+def MLNN_decision(bitcodeword, mps_device):
+    tensor_1 = torch.tensor(1, dtype=torch.float, device=mps_device)
+    tensor_0 = torch.tensor(0, dtype=torch.float, device=mps_device)
+    estimated_bits = torch.where(bitcodeword >= 0.5, tensor_1, tensor_0)
+
+    return estimated_bits
+
+
 # Single-label Neural Network Decoder:
 class DecimaltoBinary(nn.Module):
     def __init__(self, device):
@@ -26,24 +36,29 @@ class DecimaltoBinary(nn.Module):
 
 
     def forward(self, decimal_tensor):
-        decimal_tensor = decimal_tensor.unsqueeze(1)
-        decimal_tensor = torch.argmax(decimal_tensor, dim=2)
+        decimal_tensor = decimal_tensor
         Binary_output = self.B[decimal_tensor]
 
         return Binary_output
 
-def BinarytoDecimal(binary_tensor, device):
-    decimal_values = torch.sum(binary_tensor * (2 ** torch.arange(binary_tensor.shape[-1], dtype=torch.float, device=device)), dim=-1)
-    decimal_values = decimal_values.squeeze()
 
+def BinarytoDecimal(binary_tensor, device):
+    binary_tensor = binary_tensor.squeeze()  # Remove extra dimensions
+    powers_of_two = 2 ** torch.arange(binary_tensor.size(-1) - 1, -1, -1, dtype=torch.float, device=binary_tensor.device)
+    decimal_values = torch.sum(binary_tensor * powers_of_two, dim=-1)
     return decimal_values
 
 
-# Multi-label Neural Network Decoder:
-def MLNN_decision(bitcodeword, mps_device):
-    tensor_1 = torch.tensor(1, dtype=torch.float, device=mps_device)
-    tensor_0 = torch.tensor(0, dtype=torch.float, device=mps_device)
-    estimated_bits = torch.where(bitcodeword >= 0.5, tensor_1, tensor_0)
+# device = torch.device("cpu")
+# transmitted_bits = torch.tensor([[[1, 0, 1, 0]], [[0, 0, 1, 0,]]], dtype=torch.int, device=device)
+# BinarytoDecimal_result = BinarytoDecimal(transmitted_bits, device)
+# print("BinarytoDecimal", BinarytoDecimal_result)
 
-    return estimated_bits
 
+# test = torch.tensor([[2], [1]])
+# # test = torch.randint(0,16,size=(10,1), dtype=torch.int, device=device)
+#
+# Decimal_Binary =DecimaltoBinary(device)
+# DecimalBinary_result = Decimal_Binary(test)
+# print("input", test.shape)
+# print("Decimal_Binary", DecimalBinary_result.shape)
