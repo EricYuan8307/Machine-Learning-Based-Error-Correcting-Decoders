b499d280921bde457c0a363ed4f53b3fd68b7cb2
Eric Yuan-Mac <etone61eric@outlook.com>
2024-01-24 20:05:03 +0100

    save the new result to compare with SDML and MLNN in article

---
+++ Result/BER/PrintBER_SLNN.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+
+# Data Storage Reference:
+SNR = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+
+article_BPSK = [0.08, 0.07, 0.057, 0.047, 0.04, 0.0305, 0.023,
+                0.019, 0.014, 0.009, 0.0062, 0.004, 0.0025, 0.0016,
+                0.0007, 0.00041, 0.0002, 0, 0, 0, 0]
+article_SDML = [0.08, 0.065, 0.059, 0.04, 0.029, 0.0205, 0.014, 0.008, 0.007,
+        0.0031, 0.00175, 8.1e-04, 3.9e-04, 1.6e-04, 5.5e-05, 4.0e-06,
+        0, 0, 0, 0, 0]
+
+
+uncoded_BPSK = [0.07864588, 0.0670532, 0.05630509, 0.046395885, 0.03750538, 0.02963852, 0.02287882,
+                0.01716848, 0.012505335, 0.00879166, 0.00595442, 0.003866035, 0.00238995, 0.00140195,
+                0.00076981, 0.000398745, 0.000193015, 8.405e-05, 3.335e-05, 1.21e-05, 3.855e-06]
+SDML = [0, 0, 0, 0, 0, 0, 0, 0, # 0~3.5
+        0, 0, 0, 0, 0, 0, # 4~6.5
+        0, 0, 0, 0, 0, 0, 0]
+
+MLNN = [0, 0, 0, 0, 0, 0, 0, 0, # 0~3.5
+        0, 0, 0, 0, 0, 0, # 4~6.5
+        0, 0, 0, 0, 0, 0, 0]
+
+
+def nnplot():
+    plt.figure(figsize=(10, 10))
+    plt.semilogy(SNR, uncoded_BPSK, marker='.', label='uncoded_BPSK')
+    plt.semilogy(SNR, SDML, marker='x', label='SDML')
+    # plt.semilogy(SNR, article_BPSK, marker='.', label='article_BPSK')
+    # plt.semilogy(SNR, article_SDML, marker='x', label='SDML')
+    plt.semilogy(SNR, MLNN, marker='.', label='MLNN')
+
+    plt.xlabel('SNR')
+    plt.ylabel('BER')
+    plt.title('BER Estimation')
+    plt.legend(['uncoded_BPSK', 'SDML', 'MLNN'], loc='lower left')
+    # plt.legend(['uncoded_BPSK', 'SDML','article_BPSK', 'article_SDML'], loc='lower left')
+
+    # Display the Plot
+    plt.show()
+
+def main():
+    # originalplot()
+    nnplot()
+
+if __name__ == '__main__':
+    main()
