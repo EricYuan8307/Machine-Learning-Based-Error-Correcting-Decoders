27f8c023c4714bef4e5d134960602448a9767f17
Eric Yuan-HPC <y.yuan@student.tue.nl>
2024-01-24 22:08:34 +0100

    Modify Noise measurement for BPSK.

--- Transmit/NoiseMeasure.py
+++ Transmit/NoiseMeasure.py
@@ -8,7 +8,7 @@ def NoiseMeasure(noised_signal, modulated_signal):
     noise_power = torch.mean(ch_noise ** 2)
 
     # Calculate practical SNR
-    practical_snr = 10 * torch.log10(1 / (noise_power * 2.0)) + 10 * torch.log10(torch.tensor(4 / 7, dtype=torch.float))
+    practical_snr = 10 * torch.log10(1 / (noise_power * 2.0)) - 10 * torch.log10(torch.tensor(4 / 7, dtype=torch.float))
 
     return practical_snr
 
