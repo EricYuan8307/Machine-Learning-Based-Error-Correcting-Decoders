4a267c085e2d147b329668027ea6d08c81c59f7c
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-06 22:10:28 +0800

    Parity(10,5) does not work

--- Decode/Decoder.py
+++ Decode/Decoder.py
@@ -14,13 +14,40 @@ class Hamming74decoder(torch.nn.Module):
         """
 
         super(Hamming74decoder, self).__init__()
-        self.r = torch.tensor([[0, 0, 1, 0, 0, 0, 0],
+        self.H = torch.tensor([[0, 0, 1, 0, 0, 0, 0],
                                 [0, 0, 0, 0, 1, 0, 0],
                                 [0, 0, 0, 0, 0, 1, 0],
                                 [0, 0, 0, 0, 0, 0, 1], ],device=mps_device, dtype=torch.float)
 
     def forward(self, input):
 
-        result = torch.matmul(input.to(torch.float), self.r.T).to(torch.int)
+        result = torch.matmul(input, self.H.T).to(torch.int)
 
         return result
+
+
+class Parity10_5decoder(torch.nn.Module):
+    def __init__(self, mps_device):
+        """
+        hard-decision Maximum Likelihood Estimation
+
+        Args:
+            r: decoder matrix.
+            mps_device: Move Data on Specific device for computing(GPU).
+
+        Returns:
+            result: The final 4-bit codeword.
+        """
+
+        super(Parity10_5decoder, self).__init__()
+        self.H = torch.tensor([[1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                              [1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+                              [0, 1, 1, 0, 0, 0, 0, 1, 0, 0],
+                              [0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+                              [0, 0, 0, 1, 1, 0, 0, 0, 0, 1]],device=mps_device, dtype=torch.float)
+
+    def forward(self, input):
+
+        result = torch.matmul(input, self.H.T)
+
+        return result.to(torch.int)
