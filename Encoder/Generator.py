0cc8f26af81a6e1cf1bb53eb61097c7290ed45bf
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-06 14:25:00 +0800

    Prepare to generate more bits codewords

--- Encoder/Generator.py
+++ Encoder/Generator.py
@@ -1,6 +1,6 @@
 import torch
 
-def generator(nr_codewords, device):
-    bits = torch.randint(0, 2, size=(nr_codewords, 1, 4), dtype=torch.float, device=device)
+def generator(nr_codewords, bits, device):
+    codewords = torch.randint(0, 2, size=(nr_codewords, 1, bits), dtype=torch.float, device=device)
 
-    return bits
+    return codewords
