e9dda4c2093770abf212412b82f45b2c8bfaae48
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-07 16:04:04 +0800

    Parity(16,5) decoder

--- Decode/Decoder.py
+++ Decode/Decoder.py
@@ -25,7 +25,6 @@ class Hamming74decoder(torch.nn.Module):
 
         return result
 
-
 class Parity10_5decoder(torch.nn.Module):
     def __init__(self, mps_device):
         """
@@ -45,6 +44,33 @@ class Parity10_5decoder(torch.nn.Module):
                               [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]],device=mps_device, dtype=torch.float)
+
+    def forward(self, input):
+
+        result = torch.matmul(input, self.H.T)
+        result_mod2 = result % 2
+
+        return result_mod2.to(torch.int)
+
+class Parity16_5decoder(torch.nn.Module):
+    def __init__(self, mps_device):
+        """
+        hard-decision Maximum Likelihood Estimation
+
+        Args:
+            r: decoder matrix.
+            mps_device: Move Data on Specific device for computing(GPU).
+
+        Returns:
+            result: The final 4-bit codeword.
+        """
+
+        super(Parity16_5decoder, self).__init__()
+        self.H = torch.tensor([[1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+                              [0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+                              [0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1],
+                              [0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0],
+                              [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0]],device=mps_device, dtype=torch.float)
 
     def forward(self, input):
 
