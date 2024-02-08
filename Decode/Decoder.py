30026c133f617a7816775c1e76ff54cdc1410dc5
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-08 13:12:17 +0800

    Parity(20,7) Decoder

--- Decode/Decoder.py
+++ Decode/Decoder.py
@@ -21,9 +21,9 @@ class Hamming74decoder(torch.nn.Module):
 
     def forward(self, input):
 
-        result = torch.matmul(input, self.H.T).to(torch.int)
+        result = torch.matmul(input, self.H.T)
 
-        return result
+        return result.to(torch.int)
 
 class Parity10_5decoder(torch.nn.Module):
     def __init__(self, mps_device):
@@ -71,6 +71,35 @@ class Parity16_5decoder(torch.nn.Module):
                               [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                               [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],device=mps_device, dtype=torch.float)
+
+    def forward(self, input):
+
+        result = torch.matmul(input, self.H.T)
+        result_mod2 = result % 2
+
+        return result_mod2.to(torch.int)
+
+class Parity20_7decoder(torch.nn.Module):
+    def __init__(self, mps_device):
+        """
+        hard-decision Maximum Likelihood Estimation
+
+        Args:
+            r: decoder matrix.
+            mps_device: Move Data on Specific device for computing(GPU).
+
+        Returns:
+            result: The final 4-bit codeword.
+        """
+
+        super(Parity20_7decoder, self).__init__()
+        self.H = torch.tensor([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                               [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],device=mps_device, dtype=torch.float)
 
     def forward(self, input):
 
