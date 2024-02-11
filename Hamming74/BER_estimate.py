013f0229ddbbbd277da87d83e99905f96fa1f6fd
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-11 21:42:17 +0800

    File Update

--- Hamming74/BER_estimate.py
+++ Hamming74/BER_estimate.py
@@ -208,8 +208,8 @@ def main():
     # device = (torch.device("mps") if torch.backends.mps.is_available()
     #           else (torch.device("cuda") if torch.backends.cuda.is_available()
     #                 else torch.device("cpu")))
-    device = torch.device("cpu")
-    # device = torch.device("cuda")
+    # device = torch.device("cpu")
+    device = torch.device("cuda")
 
     # Hyperparameters
     num = int(1e7)
