7cb3bee7025767b0690bf49aabe8e6d80009eca5
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-11 21:40:32 +0800

    Delete NoiseMeasure74

--- Transmit/NoiseMeasure.py
+++ Transmit/NoiseMeasure.py
@@ -1,17 +1,5 @@
 import torch
 
-def NoiseMeasure74(noised_signal, modulated_signal):
-
-    # Noise Measurment
-    ch_noise = noised_signal - modulated_signal
-    # Calculate noise power
-    noise_power = torch.mean(ch_noise ** 2)
-
-    # Calculate practical SNR
-    practical_snr = 10 * torch.log10(1 / (noise_power * 2.0)) - 10 * torch.log10(torch.tensor(4 / 7, dtype=torch.float))
-
-    return practical_snr
-
 def NoiseMeasure(noised_signal, modulated_signal, origin, encoded):
 
     # Noise Measurment
