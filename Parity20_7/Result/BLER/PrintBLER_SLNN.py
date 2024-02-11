0ceb63d21fd4d433cc3aa1944a143504fdb81155
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-11 11:50:03 +0800

    File update

---
+++ Parity20_7/Result/BLER/PrintBLER_SLNN.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+
+# Data Storage BLER:
+BLER_BPSK = [0.3435, 0.2918, 0.2483, 0.2102, 0.1667, 0.1377, 0.1107, 0.0806, # 0~3.5
+             0.0528, 0.0438, 0.0298, 0.0198, 0.014, 0.0069796, 0.0038582, 0.002036318407960199, 0.00094577] # 4~8.0
+BLER_SDML = [0.2339, 0.1834, 0.1477, 0.1119, 0.0868, 0.0564, 0.0408, 0.0256, # 0~3.5
+             0.0193, 0.0105, 0.005768, 0.00301, 0.001479, 0.00071386, 0.00027525, 0.0001, 3.687708e-05] # 4~8.0
+
+BLER_SLNN_5 = [0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, # 0~3.5
+               0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0] # 4~8.0
+
+BLER_SLNN_6 = [0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, # 0~3.5
+               0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0] # 4~8.0
+
+BLER_SLNN_7 = [0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, # 0~3.5
+               0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0] # 4~8.0
+
+BLER_SLNN_8 = [0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, # 0~3.5
+               0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0] # 4~8.0
+
+BLER_SLNN_9 = [0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, # 0~3.5
+               0.0, 0.0, 0.0, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0] # 4~8.0
+
+SNR = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8.0]
+
+plt.figure(figsize=(20, 10))
+# plt.semilogy(SNR, article_SDML, marker='x', label='Soft-decision ML, Article')
+# plt.semilogy(SNR, article_BPSK, marker='+', label='BPSK, Uncoded, Article')
+plt.semilogy(SNR, BLER_SDML,  label='Soft-decision ML', color = "black")
+plt.semilogy(SNR, BLER_BPSK,  label='BPSK, Uncoded', color = "green")
+
+# plt.semilogy(SNR, BLER_SLNN_5, marker='x', label='Single-label Neural network N=5', color='blue', linestyle='--')
+# plt.semilogy(SNR, BLER_SLNN_6, marker='D', label='Single-label Neural network N=6', color='orange', linestyle='--')
+# plt.semilogy(SNR, BLER_SLNN_7, marker='o', label='Single-label Neural network N=7', color='green', linestyle='--')
+# plt.semilogy(SNR, BLER_SLNN_8, marker='v', label='Single-label Neural network N=8', color='red', linestyle='--')
+# plt.semilogy(SNR, BLER_SLNN_9, marker='<', label='Single-label Neural network N=9', color='purple', linestyle='--')
+
+plt.xlabel('SNR', fontsize=20)
+plt.ylabel('BLER', fontsize=20)
+plt.title('Parity(10,5) BLER Estimation', fontsize=20)
+# plt.legend(['SDML', 'BPSK'], loc='lower left')
+# plt.legend(['Soft-decision ML, Article', 'BPSK, Uncoded, Article', 'Soft-decision ML', 'BPSK, Uncoded','Single-label Neural network'], loc='lower left')
+plt.legend(['Soft-decision ML', 'BPSK, Uncoded',
+            # 'Single-label Neural network N=5', 'Single-label Neural network N=6', 'Single-label Neural network N=7', 'Single-label Neural network N=8', 'Single-label Neural network N=9',
+            ], loc='lower left')
+
+
+plt.show()
