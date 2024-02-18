83b98c5b5b978bccc9bfbee07a58af44284b5e4f
Eric Yuan-Mac <etone61eric@outlook.com>
2024-02-18 15:30:37 +0800

    Parity(16,5) check code result update.

--- Parity16_5/BER_estimate.py
+++ Parity16_5/BER_estimate.py
@@ -97,27 +97,27 @@ def estimation_SDML(num, bits, encoded, SNR_opt_ML, result, device):
 
 
 def main():
-    device = (torch.device("mps") if torch.backends.mps.is_available()
-              else (torch.device("cuda") if torch.backends.cuda.is_available()
-                    else torch.device("cpu")))
-    # device = torch.device("cpu")
+    # device = (torch.device("mps") if torch.backends.mps.is_available()
+    #           else (torch.device("cuda") if torch.backends.cuda.is_available()
+    #                 else torch.device("cpu")))
+    device = torch.device("cpu")
     # device = torch.device("cuda")
 
     # Hyperparameters
-    num = int(1e6)
+    num = int(5e6)
     bits = 5
     encoded = 16
     SNR_opt_BPSK = torch.arange(0, 8.5, 0.5)
 
-    SNR_opt_ML = torch.arange(0, 8.5, 0.5)
+    SNR_opt_ML = torch.arange(7.5, 8.5, 0.5)
     SNR_opt_ML = SNR_opt_ML + 10 * torch.log10(torch.tensor(bits / encoded, dtype=torch.float)) # for MLNN article
 
     result_save = np.zeros((1, len(SNR_opt_BPSK)))
-    result_BPSK = estimation_BPSK(num, bits, SNR_opt_BPSK, result_save, device)
+    # result_BPSK = estimation_BPSK(num, bits, SNR_opt_BPSK, result_save, device)
     result_SDML = estimation_SDML(num, bits, encoded, SNR_opt_ML, result_save, device)
 
     result_all = np.vstack([
-        # result_BPSK,
+        result_BPSK,
         result_SDML,
     ])
 
