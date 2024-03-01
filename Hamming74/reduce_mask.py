894be78f56fa44a1e871737e42cc93ecf87a47bb
Eric Yuan-Mac <etone61eric@outlook.com>
2024-03-01 18:58:58 +0800

    file update

--- Hamming74/reduce_mask.py
+++ Hamming74/reduce_mask.py
@@ -81,13 +81,14 @@ class MaskMatrix(torch.nn.Module):
                                 [1, 0, 0, 0, 0, 0, 1]], dtype=torch.float, device=self.device)
 
         elif edge_delete == 39 and encoded == 7 and hidden_size == 7:
-            mask = torch.tensor([[0, 0, 1, 0, 0, 1, 0],
-                                [0, 0, 0, 1, 0, 1, 0],
-                                [0, 0, 0, 0, 1, 0, 0],
-                                [0, 0, 0, 0, 0, 0, 1],
-                                [0, 1, 0, 0, 0, 0, 0],
-                                [1, 0, 1, 0, 0, 0, 0],
-                                [1, 0, 0, 0, 0, 0, 0]], dtype=torch.float, device=self.device)
+            mask = torch.tensor([
+                [0, 0, 1, 0, 0, 1, 0],
+                [0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1],
+                [0, 1, 0, 0, 0, 0, 0],
+                [1, 0, 1, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0]], dtype=torch.float, device=self.device)
 
         elif edge_delete == 40 and encoded == 7 and hidden_size == 7:
             mask = torch.tensor([[0, 0, 1, 0, 0, 1, 0],
