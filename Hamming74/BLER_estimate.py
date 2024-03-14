f611cf8ee93d756265832a6056328aec87485f48
Eric Yuan-HPC <y.yuan@student.tue.nl>
2024-03-14 20:32:38 +0100

    File Update

--- Hamming74/BLER_estimate.py
+++ Hamming74/BLER_estimate.py
@@ -111,11 +111,11 @@ def main():
     SNR_opt_ML = SNR_opt_ML + 10 * torch.log10(torch.tensor(bits / encoded, dtype=torch.float))  # for SLNN article
 
     result_save = np.zeros((1, len(SNR_opt_BPSK)))
-    # result_BPSK = estimation_BPSK(num, bits, SNR_opt_BPSK, result_save, device)
+    result_BPSK = estimation_BPSK(num, bits, SNR_opt_BPSK, result_save, device)
     result_SDML = estimation_SDML(num, bits, encoded, SNR_opt_ML, result_save, device)
 
     result_all = np.vstack([
-        # result_BPSK,
+        result_BPSK,
         result_SDML,
                             ])
 
