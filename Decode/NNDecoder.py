1d5130b3406984192a48bbc0c5e0111591e8ba38
Eric Yuan-Mac <etone61eric@outlook.com>
2024-03-19 15:56:52 +0100

    File update

--- Decode/NNDecoder.py
+++ Decode/NNDecoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class SingleLabelNNDecoder(nn.Module):
+class SingleLabelNNDecoder1(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
 
         super().__init__()
