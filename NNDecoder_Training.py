6856dffba9a5803f0c924baa942ce1c5eb0381b6
Eric Yuan-HPC <y.yuan@student.tue.nl>
2024-03-20 21:58:05 +0100

    Parity(26,10) model result

--- NNDecoder_Training.py
+++ NNDecoder_Training.py
@@ -374,7 +374,7 @@ def main():
 
     # Hyperparameters
     NeuralNetwork_type = ["SLNN", "MLNN"]
-    SLNN_hidden_size1 = [25, 26, 27, 28] # [24, 25, 26, 27, 28]
+    SLNN_hidden_size1 = [24, 25, 26, 27, 28]
     SLNN_hidden_size2 = [[25, 25], [100, 20], [20, 100], [100, 25], [25, 100]]
     MLNN_hidden_size = [[1000, 500], [2000, 1000], [2000, 1000, 500]]
     batch_size = 64
@@ -401,10 +401,10 @@ def main():
         model_save_path = f"Result/Model/{encoding_method}{encoded}_{bits}/{NN_type}_{device}/"
 
         if NN_type == "SLNN" :
-            # for i in range(len(SLNN_hidden_size1)):
-            #     model_name = f"{NN_type}_hiddenlayer{SLNN_hidden_size1[i]}"
-            #     SLNN_training1(snr, encoding_method, nr_codeword, bits, encoded, epochs, learning_rate, momentum, batch_size, SLNN_hidden_size1[i],
-            #                   model_save_path, model_name, NN_type, patience, delta, device)
+            for i in range(len(SLNN_hidden_size1)):
+                model_name = f"{NN_type}_hiddenlayer{SLNN_hidden_size1[i]}"
+                SLNN_training1(snr, encoding_method, nr_codeword, bits, encoded, epochs, learning_rate, momentum, batch_size, SLNN_hidden_size1[i],
+                              model_save_path, model_name, NN_type, patience, delta, device)
 
             for j in range(len(SLNN_hidden_size2)):
                 model_name = f"{NN_type}_hiddenlayer{SLNN_hidden_size2[j]}"
