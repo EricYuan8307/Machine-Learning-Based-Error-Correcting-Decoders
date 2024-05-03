3f3e6513d4f0b4ede99c22080d1456b7c620f6a9
Eric Yuan-Mac <etone61eric@outlook.com>
2024-05-03 21:30:20 +0200

    Testing

--- testing.py
+++ testing.py
@@ -1,90 +1,38 @@
-import torch.nn as nn
 import torch
+import random
 
-class ANN(nn.Module):
-    def __init__(self):
-        super(ANN, self).__init__()
-        self.hidden = nn.Linear(7, 16, bias=True)  # input layer: 7 neurons to hidden layer: 7 neurons
-        self.softmax = nn.Softmax(dim=1)       # Softmax activation function
-        self.output = nn.Linear(16, 4, bias=True)  # hidden layer: 7 neurons to output layer: 16 neurons
-        self.sigmoid = nn.Sigmoid()
+n=7
+k=4
+sigma = [0]
+d_model = 32
 
-    def forward(self, x):
-        x = self.hidden(x)
-        x = self.softmax(x)
-        x = self.output(x)
-        x = self.sigmoid(x)
-        return x
+def bin_to_sign(x):
+    return 2 * x - 1
 
+def sign_to_bin(x):
+    return (x + 1) / 2
 
-nr_codewords = int(1e5)
-seed = torch.tensor([941682])
-print("Seed:", seed, flush=True)
+generator_matrix = torch.tensor([[1, 1, 1, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0],
+        [1, 1, 0, 1, 0, 0, 1]], dtype=torch.float)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# device = torch.device("cpu")
+pc_matrix = torch.tensor([[1, 0, 1, 0, 1, 0, 1],
+                                 [0, 1, 1, 0, 0, 1, 1],
+                                 [0, 0, 0, 1, 1, 1, 1]], dtype=torch.float)  # Hamming(7,4) BP
 
-# Set seed
-torch.manual_seed(seed)
+m = torch.randint(0, 2, (5, 1, k)).to(torch.float)
+x = torch.matmul(m, generator_matrix) % 2
+z = torch.randn(n) * random.choice(sigma)
+y = bin_to_sign(x) + z
+magnitude = torch.abs(y)
+binary = sign_to_bin(torch.sign(y))
+syndrome = torch.matmul(binary, pc_matrix.T) % 2
+syndrome1 = bin_to_sign(syndrome)
 
-# Load model parameters
-loaded_model = torch.load('Result/Model/Hamming7_4/MLNN_cpu/MLNN_hiddenlayer16.pth')
+emb = torch.cat([magnitude, syndrome1], 2)
 
-# ANN Model
-model_ann = ANN()
-model_ann.load_state_dict(loaded_model)
-# model_ann.to(device)
-model_ann.eval()
+src_embed = torch.nn.Parameter(torch.empty((n + pc_matrix.size(0), d_model)))
 
-# SNRs (dB)
-SNRs = torch.arange(-5, 7.5, 0.5)
-
-# Generator Matrix
-G = torch.tensor([[1, 1, 1, 0, 0, 0, 0], [1, 0, 0, 1, 1, 0, 0], [0, 1, 0, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]])
-
-# Number of codewords
-multi = torch.zeros(len(SNRs))
-ann_ber = torch.zeros(len(SNRs))
-
-for idx_SNR, SNR in enumerate(SNRs):
-    SNRlin = 10 ** (SNR / 10)
-
-    errors_ann = 0
-
-    while (errors_ann < 1000):
-        multi[idx_SNR] += 1
-
-        # Generation of the information sequence
-        bits_info = torch.randint(0, 2, (nr_codewords, 4))
-
-        # Encode
-        codewords = torch.matmul(bits_info, G) % 2
-
-        # BPSK modulation
-        s = 2 * codewords - 1
-
-        # AWGN Channel
-        # Noise generation
-        noise = torch.randn(s.shape)
-
-        # Scale the noise power
-        n = torch.sqrt(1 / (2 * SNRlin)) * noise
-
-        # Add AWGN
-        y = s + n
-
-        # Apply model
-        # y = y.to(device)
-
-        with torch.no_grad():
-            predictions_ann = model_ann(y)
-
-        # Calculate the bit errors
-        predicted_ann = predictions_ann > 0.5
-
-        errors_ann += (predicted_ann.detach().cpu() != bits_info).sum().sum()
-
-    ann_ber[idx_SNR] = errors_ann / (multi[idx_SNR] * nr_codewords * 4)
-    print("SNR: ", SNR.item(), " ANN BER: ", ann_ber[idx_SNR].item(), flush=True)
-
-print("ANN BER: ", ann_ber, flush=True)
+emb = torch.matmul(emb, src_embed)
+print(emb.shape)
