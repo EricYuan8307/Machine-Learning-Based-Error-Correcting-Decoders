bbf2c800655957e83225b3d39877a4e6c43359f5
Eric Yuan-Mac <etone61eric@outlook.com>
2024-05-06 09:16:51 +0200

    Model Store Place Move

--- Result/Hamming7_4/BER/PrintBER_MLNN.py
+++ Result/Hamming7_4/BER/PrintBER_MLNN.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 # Data Storage Reference:
 SNR = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8.0]
 
-article_BPSK = [0.08, 0.07, 0.057, 0.047, 0.04, 0.0305, 0.023, 0.019, # 0~3.5
-                0.014, 0.009, 0.0062, 0.004, 0.0025, 0.0016,0.0007, 0.00041, 0.0002]
-article_SDML = [0.08, 0.065, 0.059, 0.04, 0.029, 0.0205, 0.014, 0.008, 0.0055, # 0~4
-                0.0031, 0.00175, 8.1e-04, 3.9e-04, 1.6e-04, 5.5e-05, 1.4e-05]# 4.5~7.5
-article_MLNN_100 = [0.08, 0.065, 0.057, 0.044, 0.03, 0.023, 0.016, 0.01, 0.007, # 0~4
-                0.0041, 0.0028, 0.0013, 0.00075, 0.000395, 1.8e-04, 7.0e-05, # 4.5~7.5
-                3.0e-05]
+# article_BPSK = [0.08, 0.07, 0.057, 0.047, 0.04, 0.0305, 0.023, 0.019, # 0~3.5
+#                 0.014, 0.009, 0.0062, 0.004, 0.0025, 0.0016,0.0007, 0.00041, 0.0002]
+# article_SDML = [0.08, 0.065, 0.059, 0.04, 0.029, 0.0205, 0.014, 0.008, 0.0055, # 0~4
+#                 0.0031, 0.00175, 8.1e-04, 3.9e-04, 1.6e-04, 5.5e-05, 1.4e-05]# 4.5~7.5
+# article_MLNN_100 = [0.08, 0.065, 0.057, 0.044, 0.03, 0.023, 0.016, 0.01, 0.007, # 0~4
+#                 0.0041, 0.0028, 0.0013, 0.00075, 0.000395, 1.8e-04, 7.0e-05, # 4.5~7.5
+#                 3.0e-05]
 
 
 BER_uncoded_BPSK = [0.07864588, 0.0670532, 0.05630509, 0.046395885, 0.03750538, 0.02963852, 0.02287882, 0.01716848, # 0~3.5
@@ -37,7 +37,7 @@ def nnplot():
     plt.semilogy(SNR, BER_SDML, label='Soft-decision ML', color = "black")
     # plt.semilogy(SNR, article_BPSK, marker='.', label='BPSK, Uncoded, Article')
     # plt.semilogy(SNR, article_SDML, marker='x', label='Soft-decision ML, Article')
-    plt.semilogy(SNR, article_MLNN_100, marker='D', label='N=100, Article', color = "pink", linestyle='--')
+    # plt.semilogy(SNR, article_MLNN_100, marker='D', label='N=100, Article', color = "pink", linestyle='--')
     plt.semilogy(SNR, BER_MLNN_100, marker='D', label='N=100', color = "pink", linestyle='--')
     plt.semilogy(SNR, BER_MLNN_50_50, marker='o', label='N1=50, N2=50', color = "orange", linestyle='--')
     plt.semilogy(SNR, BER_MLNN_100_100, marker='v', label='N1=100, N2=100', color = "red", linestyle='--')
@@ -49,7 +49,7 @@ def nnplot():
     plt.title('Hamming(7,4) Multi-label Neural Network BER Estimation', fontsize=20)
     # plt.legend(['uncoded_BPSK', 'SDML', 'MLNN'], loc='lower left')
     # plt.legend(['BPSK, Uncoded', 'Soft-decision ML', 'BPSK, Uncoded, Article', 'Soft-decision ML, Article', 'Multi-label Neural Network'], loc='lower left')
-    plt.legend(['BPSK, Uncoded', 'Soft-decision ML', 'N=100, Article', 'N=100', 'N1=50, N2=50', 'N1=100, N2=100', 'N=16'], loc='lower left')
+    plt.legend(['BPSK, Uncoded', 'Soft-decision ML', 'N=100', 'N1=50, N2=50', 'N1=100, N2=100', 'N=16'], loc='lower left')
     # Display the Plot
     plt.show()
 
