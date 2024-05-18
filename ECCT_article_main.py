afe37e08d408cb4160a98fa06f3dcb170d541134
Eric Yuan-Mac <etone61eric@outlook.com>
2024-05-19 00:04:11 +0200

    Early stopping make stopped too early

--- ECCT_article_main.py
+++ ECCT_article_main.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--test_batch_size', type=int, default=2048)
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--patience', type=int, default=10)
+    parser.add_argument('--patience', type=int, default=100)
     parser.add_argument('--delta', type=float, default=0.001)
 
     # Code args
