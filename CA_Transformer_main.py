003613ce2f59acd1be41c09f6962b4ac57e78f84
Eric Yuan-Mac <etone61eric@outlook.com>
2024-06-19 14:01:11 +0200

    File update

--- CA_Transformer_main.py
+++ CA_Transformer_main.py
@@ -178,10 +178,10 @@ if __name__ == '__main__':
     class Code():
         pass
     code = Code()
-    device = (torch.device("mps") if torch.backends.mps.is_available()
-              else (torch.device("cuda") if torch.cuda.is_available()
-                    else torch.device("cpu")))
-    # device = torch.device("cuda")
+    # device = (torch.device("mps") if torch.backends.mps.is_available()
+    #           else (torch.device("cuda") if torch.cuda.is_available()
+    #                 else torch.device("cpu")))
+    device = torch.device("cuda")
     # device = torch.device("cpu")
     code.k = args.code_k
     code.n = args.code_n
