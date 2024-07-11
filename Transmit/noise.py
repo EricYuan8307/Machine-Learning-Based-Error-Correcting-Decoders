e412d27a5e914da48b1b1030e72ce2457e06a396
Eric Yuan-Mac <etone61eric@outlook.com>
2024-07-11 11:08:31 +0800

    Prepare for ISD

--- Transmit/noise.py
+++ Transmit/noise.py
@@ -14,3 +14,11 @@ def AWGN_ECCT(modulated_signal, std, device):
     noised_signal = modulated_signal + noise
 
     return noised_signal
+
+def AWGN_ISD(signal, snr_dB, device):
+    # Add Gaussian noise to the signal
+    noise_power = 10 ** (snr_dB / 10)
+    noise = torch.sqrt(1 / (2 * noise_power)) * torch.randn(signal.shape, device=device)
+    noised_signal = signal + noise
+
+    return noised_signal, noise
